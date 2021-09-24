65614db91f25ef6a29c2fad9154ca6f1abf918db
Raghu <raghunandan.mrn82@gmail.com>
2021-09-23 20:18:00 -0700

    modified the output format for str_interleave

--- str_interleave.py
+++ str_interleave.py
@@ -7,9 +7,8 @@ def interleave_string(str1,str2):
 	str1 - first string that needs to be intere=leaved
 	str2 - second string that needs to be interleaved
 	'''
-	print(f"Strings to interleave - string 1 = {str1}, string 2 = {str2}")
 	small_string,bigstring = (str1,str2) if len(str1) <= len(str2) else (str2,str1)
-	print("interleaved string = " + "".join(str1[i]+str2[i] for i in range(0,len(small_string))) + bigstring[len(small_string)::])
+	print(f"string 1 = {str1}, string 2 = {str2}." + " Interleaved string = " + "".join(str1[i]+str2[i] for i in range(0,len(small_string))) + bigstring[len(small_string)::])
 
 if __name__ == "__main__":
 	interleave_string("AAAA","BBBB")
