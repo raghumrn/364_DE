26c045e5c819133c9500ff1bc0f9d92905c5b995
Raghu <raghunandan.mrn82@gmail.com>
2021-09-23 19:39:00 -0700

    reading inputs from a file in subfolder

--- str_manipulator.py
+++ str_manipulator.py
@@ -1,7 +1,21 @@
+import configparser
+
 from str_reverse import *
 from str_interleave import *
 
 if __name__ == "__main__":
 
-	reverse_string("ABCDEF")
-	interleave_string("AAAA","BBBB")
+	# reverse_string("ABCDEF")
+	# interleave_string("AAAA","BBBB")
+
+	config_file=r"conf\inputs.cfg"
+
+	#reading inputs from config file 
+	raw_cfg_parser = configparser.RawConfigParser()
+	raw_cfg_parser.read(config_file)
+	string_to_reverse = raw_cfg_parser.get('INPUTS','string_to_reverse')
+	first_string_to_interleave = raw_cfg_parser.get('INPUTS','first_string_to_interleave')
+	second_string_to_interleave = raw_cfg_parser.get('INPUTS','second_string_to_interleave')
+
+	reverse_string(string_to_reverse)
+	interleave_string(first_string_to_interleave,second_string_to_interleave)	
