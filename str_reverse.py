1d4540286d4136e44fde0b90638a56838372abd9
Raghu <raghunandan.mrn82@gmail.com>
2021-09-23 18:46:23 -0700

    Added a function to reverse a string

---
+++ str_reverse.py
@@ -0,0 +1,15 @@
+def reverse_string(ip_string):
+	'''
+	This function reverses the given string 
+
+	parameters:
+	-------------
+	ip_string - input string to reverse
+	'''
+
+	print(f"reverse of {ip_string} is {ip_string[::-1]}")
+
+
+if __name__ == "__main__":
+	reverse_string('ABCDEF')
+
